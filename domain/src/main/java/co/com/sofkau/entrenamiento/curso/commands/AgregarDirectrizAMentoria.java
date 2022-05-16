2c5bedb86dbe9d25b7e275822c83444840a206fb
Camila Morales <camila.moralesg21@gmail.com>
2022-05-16 15:35:34 -0500

    caso de uso directriz --- Se crea el caso de uso de agregar directriz

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizAMentoria.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizAMentoria.java
@@ -1,18 +1,23 @@
 package co.com.sofkau.entrenamiento.curso.commands;
 
 import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
 
 public class AgregarDirectrizAMentoria extends Command {
 
+    private final CursoId cursoId;
     private final MentoriaId mentoriaId;
     private final Directiz directiz;
 
-    public AgregarDirectrizAMentoria(MentoriaId mentoriaId, Directiz directiz) {
+    public AgregarDirectrizAMentoria(CursoId cursoId, MentoriaId mentoriaId, Directiz directiz) {
+        this.cursoId = cursoId;
         this.mentoriaId = mentoriaId;
         this.directiz = directiz;
     }
+
+    public CursoId getCursoId() {return cursoId;}
 
     public MentoriaId getMentoriaId() {
         return mentoriaId;
